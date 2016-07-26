4e888558bd76803781cc5c2120331780c9a16c8e
Mark Dawson <medawson94@gmail.com>
2016-07-26 10:24:40 +0400

    all staff view

--- orders/forms.py
+++ orders/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 import datetime
-from functools import partial
-
-DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 MENU_ITEM_QUANTITY_CHOICES = [(i, str(i)) for i in range(1,11)]
 
 class OrderAddMenuItemForm(forms.Form):
 	quantity = forms.TypedChoiceField(choices = MENU_ITEM_QUANTITY_CHOICES,
-										coerce=int)
-	date = forms.DateField(initial = datetime.date.today())
+										coerce=int) 
+	date = forms.DateField(initial = datetime.date.today(),
+							 widget=forms.TextInput(attrs={'type': 'date'}))
+
+	def clean_date(self):
+		cd = self.cleaned_data
+		if cd['date'] < datetime.date.today():
+			raise forms.ValidationError("The date cannot be in the past.")
+		return cd['date']
--- orders/views.py
+++ orders/views.py
@@ -10,12 +10,10 @@ import datetime
 @require_POST
 def order_add(request, menu_item_id):
 	menu_item = get_object_or_404(MenuItem, id=menu_item_id)
-	form = OrderAddMenuItemForm(request.POST)
-	if form.is_valid():
-		cd = form.cleaned_data
+	menu_item_form = OrderAddMenuItemForm(request.POST)
+	if menu_item_form.is_valid():
+		cd = menu_item_form.cleaned_data
 		date = cd['date']
-		if date < datetime.date.today():
-			raise forms.ValidationError("The date cannot be in the past.")
 		item = MenuItem.objects.get(id=menu_item_id)
 		new_order = OrderItem.objects.create(user=request.user,
 								menu_item=item,
@@ -26,11 +24,17 @@ def order_add(request, menu_item_id):
 		print('item created')
 		return redirect('orders:upcoming_orders')
 	else:
-		raise forms.ValidationError("form not valid - unknown reason")
+		return render(request,'menu/menu_detail.html',
+					{'menu_item': menu_item,
+					'menu_item_form': menu_item_form})
 
 def order_remove(request, order_id):
 	OrderItem.objects.get(id=order_id).delete()
 	return redirect('orders:upcoming_orders')
+
+def order_remove_all_staff_list(request, order_id):
+	OrderItem.objects.get(id=order_id).delete()
+	return redirect('orders:all_staff_orders')
 
 def upcoming_orders(request):
 	now = datetime.datetime.now()
@@ -42,8 +46,12 @@ def upcoming_orders(request):
 
 def past_orders(request):
 	now = datetime.datetime.now()
-	orders = orders = OrderItem.objects.filter(user=request.user,
+	orders = OrderItem.objects.filter(user=request.user,
 								date__lt=now).order_by('-date')[:15]
 	return render(request, 'orders/orders_list.html',
 				 { "orders" : orders,
 					"title_message": 'Past Orders'})
+
+def all_staff_orders(request):
+	orders = OrderItem.objects.all()
+	return render(request, 'orders/all_staff_orders_list.html', {'orders':orders})
